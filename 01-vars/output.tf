2897b23cb3835f71c1fb36d8eed9e7fe1b72a752
sivarao <siva.nani4@gmail.com>
2023-06-30 12:50:40 +0530

    file changes

---
+++ 01-vars/output.tf
@@ -0,0 +1,11 @@
+output "sample_list" {
+  value = var.sample_list[1]
+}
+
+output "sample_string" {
+  value = var.sample_string
+}
+
+output "sample_number" {
+  value = var.sample_number
+}
--- 01-vars/vars.tf
+++ 01-vars/vars.tf
@@ -17,15 +17,3 @@ variable "sample_list" {
   
 }
 
-output "sample_list" {
-  value = var.sample_list[1]
-}
-
-
-output "sample_string" {
-  value = var.sample_string
-}
-
-output "sample_number" {
-  value = var.sample_number
-}
