fdd2cdb34cb325ce49d8060b86919f9d2629a891
sivarao <siva.nani4@gmail.com>
2023-06-30 12:41:13 +0530

    varschanges

--- 01-vars/vars.tf
+++ 01-vars/vars.tf
@@ -6,6 +6,22 @@ variable "sample_string" {
   default = "hello world"
 }
 
+variable "sample_list" {
+  default = [
+    100,
+    2.5,
+    "hello",
+    true,
+    123
+  ]
+  
+}
+
+output "sample_list" {
+  value = var.sample_list[1]
+}
+
+
 output "sample_string" {
   value = var.sample_string
 }
